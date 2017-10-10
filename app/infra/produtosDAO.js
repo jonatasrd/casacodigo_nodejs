efd67848ba23362edec4f5e06635be1a9f2d0aa6
Jonatas Ramos <jonatasrd@gmail.com>
2017-10-09 22:06:16 -0300

    inclusão da rota para cadastrar os produtos

--- app/infra/produtosDAO.js
+++ app/infra/produtosDAO.js
@@ -3,7 +3,11 @@ function ProdutosDAO(connection){
 }
 
 ProdutosDAO.prototype.lista = function(callback){
-    this._connection.query('select * from produtos',callback)
+    this._connection.query('select * from produtos',callback);
+}
+
+ProdutosDAO.prototype.salva = function(produto,callback) {
+    this._connection.query('INSERT INTO produtos SET ?', produto, callback);
 }
 
 module.exports = function(){
--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -3,12 +3,32 @@ module.exports = function(app){
 
     app.get('/produtos', function(req,res){
         var connection = app.infra.connectionFactory();
-        var produtosBanco = new app.infra.produtosDAO(connection);
+        var produtosDAO = new app.infra.produtosDAO(connection);
 
-        produtosBanco.lista(function(err, results){
+        produtosDAO.lista(function(err, results){
             res.render('produtos/lista',{lista:results});
         });
 
         connection.end();
+    });
+
+    app.get('/produtos/cadastro',function(req, res){
+        res.render('produtos/form');
+    });
+
+    app.post('/produtos/salva',function(req,res) {
+        var produto = req.body;
+        //console.log(produto);
+
+        var connection = app.infra.connectionFactory();
+        var produtosDAO = new app.infra.produtosDAO(connection);
+
+        produtosDAO.salva(produto,function(erros,resultado){
+            console.log(erros);
+            res.render('produtos/lista');
+        });
+
+        connection.end();
+        
     });
 }
