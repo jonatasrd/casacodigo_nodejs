c2b35f0c23e50ece59eadab7b2f6820eb56b45e7
Jonatas Ramos <jonatasrd@gmail.com>
2017-11-16 22:25:26 -0200

    adicionado middleware

--- config/express.js
+++ config/express.js
@@ -13,11 +13,20 @@ module.exports = function() {
     app.use(bodyParser.urlencoded({extended: true}));
     app.use(bodyParser.json());
     app.use(expressValidator());
+
+    load('routes', {cwd: 'app', verbose:true})
+    .then('infra')
+    .into(app);
     
-
-    load('routes', {cwd: 'app'})
-        .then('infra')
-        .into(app);
-
+    app.use(function(req, res, next){
+        res.status(404).render('erros/404');
+        next();
+    });
+    
+    app.use(function(error, req, res, next){
+        res.status(500).render('erros/500');
+        next();
+    });
+    
     return app;
 };
